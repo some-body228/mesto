7408683b8eae5e2aa4d31fa36556aa433ae21f51
KA3AX <Some-body228@yandex.ru>
2020-07-17 17:29:32 +0400

    открытие закрытие попапа реализовать одной функцией

--- scripts/script.js
+++ scripts/script.js
@@ -7,13 +7,14 @@ let saveBtn = document.querySelector(".popup__save-button");
 let form = document.querySelector(".popup__form");
 let popupName = document.querySelector(".popup__input[name='user-name']");
 let popupCap = document.querySelector(".popup__input[name='user-caption']");
-function popupOpen(){
+function popupOpenClose(){
+    if (popup.classList.length < 2){
     popup.classList.add("popup_opened");
     popupName.value = name.textContent;
     popupCap.value = cap.textContent;
-}
-function popupClose(){
-    popup.classList.remove("popup_opened");
+    } else {
+        popup.classList.remove("popup_opened");
+    }
 }
 function formSubmitHandler(evt) {
     evt.preventDefault();
@@ -22,8 +23,8 @@ function formSubmitHandler(evt) {
     popupClose();
 
 }
-openBtn.addEventListener("click", popupOpen);
-exitBtn.addEventListener("click", popupClose);
+openBtn.addEventListener("click", popupOpenClose);
+exitBtn.addEventListener("click", popupOpenClose);
 form.addEventListener("submit", formSubmitHandler);
 
 
