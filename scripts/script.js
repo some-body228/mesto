a6637b94777dc0f1237f3759ee10f22b0935c3c8
KA3AX <Some-body228@yandex.ru>
2020-07-17 17:19:16 +0400

    исправить ширину блоков

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,25 @@
 let  name = document.querySelector(".profile__user-name");
 let cap = document.querySelector(".profile__user-caption");
-let openBtn = document.querySelector(".profile__redaction-button")
-let popup = document.querySelector(".popup")
-let exitBtn = document.querySelector(".popup__exit-button")
-let saveBtn = document.querySelector(".popup__save-button")
-let form = document.querySelector(".popup__form")
+let openBtn = document.querySelector(".profile__redaction-button");
+let popup = document.querySelector(".popup");
+let exitBtn = document.querySelector(".popup__exit-button");
+let saveBtn = document.querySelector(".popup__save-button");
+let form = document.querySelector(".popup__form");
+let popupName = document.querySelector(".popup__input[name='user-name']");
+let popupCap = document.querySelector(".popup__input[name='user-caption']");
 function popupOpen(){
     popup.classList.add("popup_opened");
-    let popup_input = popup.querySelectorAll(".popup__input");
-    popup_input[0].value = name.textContent;
-    popup_input[1].value = cap.textContent;
+    popupName.value = name.textContent;
+    popupCap.value = cap.textContent;
 }
 function popupClose(){
     popup.classList.remove("popup_opened");
 }
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    
-    let popupName = document.querySelector(".popup__input[name='user-name']");
-    let popupCap = document.querySelector(".popup__input[name='user-caption']");
-    name.textContent=popupName.value
-    cap.textContent=popupCap.value
-    popupClose()
+    name.textContent=popupName.value;
+    cap.textContent=popupCap.value;
+    popupClose();
 
 }
 openBtn.addEventListener("click", popupOpen);
