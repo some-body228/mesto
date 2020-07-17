30047831dd128647c16e6b457ecc4749f4f7a0f3
KA3AX <Some-body228@yandex.ru>
2020-07-17 13:00:51 +0400

    доработать

--- scripts/script.js
+++ scripts/script.js
@@ -5,21 +5,11 @@ let popup = document.querySelector(".popup")
 let exitBtn = document.querySelector(".popup__exit-button")
 let saveBtn = document.querySelector(".popup__save-button")
 let form = document.querySelector(".popup__form")
-function popupOpenClose(){
-    if (popup.classList.length > 1){
+function popupOpen(){
     popup.classList.add("popup_opened");
     let popup_input = popup.querySelectorAll(".popup__input");
     popup_input[0].value = name.textContent;
     popup_input[1].value = cap.textContent;
-    } else {
-        evt.preventDefault();
-    
-        let popupName = document.querySelector(".popup__input[name='user-name']");
-        let popupCap = document.querySelector(".popup__input[name='user-caption']");
-        name.textContent=popupName.value
-        cap.textContent=popupCap.value
-        popupClose()
-    }
 }
 function popupClose(){
     popup.classList.remove("popup_opened");
@@ -34,8 +24,8 @@ function formSubmitHandler(evt) {
     popupClose()
 
 }
-openBtn.addEventListener("click", popupOpenClose);
-exitBtn.addEventListener("click", popupOpenClose);
+openBtn.addEventListener("click", popupOpen);
+exitBtn.addEventListener("click", popupClose);
 form.addEventListener("submit", formSubmitHandler);
 
 
